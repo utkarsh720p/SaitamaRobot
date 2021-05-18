97fb394ef13463f6bad3939c4ad8a8554f83eda6
Itachi uchiha <80628218+utkarsh720p@users.noreply.github.com>
2021-05-18 12:17:56 +0530

    Update Reverse.py

--- SaitamaRobot/modules/Reverse.py
+++ SaitamaRobot/modules/Reverse.py
@@ -14,7 +14,7 @@ from telegram.ext import CallbackContext, run_async
 from SaitamaRobot import dispatcher
 
 from SaitamaRobot.modules.disable import DisableAbleCommandHandler
-from Saitamabot.modules.helper_funcs.alternate import typing_action
+from SaitamaRobot.modules.helper_funcs.alternate import typing_action
 
 opener = urllib.request.build_opener()
 useragent = 'Mozilla/5.0 (Linux; Android 6.0.1; SM-G920V Build/MMB29K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/52.0.2743.98 Mobile Safari/537.36'
